596000ced678d6ceed8b9fde5f9a998423aa47fe
honglou2001 <honglou2001@qq.com>
2014-09-26 13:41:24 +0800

    add data
    add data

---
+++ activitydemo/src/com/example/domain/CommunicationSharePostData.java
@@ -0,0 +1,19 @@
+package com.example.domain;
+
+public class CommunicationSharePostData {
+	private boolean fIsSuccess;//返回是否成功
+	private String fInfo;//返回内容
+	public boolean isfIsSuccess() {
+		return fIsSuccess;
+	}
+	public void setfIsSuccess(boolean fIsSuccess) {
+		this.fIsSuccess = fIsSuccess;
+	}
+	public String getfInfo() {
+		return fInfo;
+	}
+	public void setfInfo(String fInfo) {
+		this.fInfo = fInfo;
+	}
+
+}
